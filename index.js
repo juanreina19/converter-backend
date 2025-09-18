eae4c87007ca622c3a7527f67be67369ccb41dec
juanreina <juandavidreina.ms@gmail.com>
2025-09-18 13:12:06 -0500

    endpoint status back

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.use(cors());
 
 app.use("/api/converter", router);
 
+app.get('/health', (req, res) => {
+    res.status(200).send({ status: 'API working correctly!' })
+})
+
 app.listen(3500, () => {
     console.log("Listening at port 3500");
 })
